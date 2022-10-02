587347c2a60419456fc7115fca712554bd063d60
benmlahzied <benmlahzied@gmail.com>
2022-10-02 10:40:38 +0200

    Initial commit

---
+++ src/main/java/com/zbm/demo/sonarcloud/SonarCloudDemoApplication.java
@@ -0,0 +1,13 @@
+package com.zbm.demo.sonarcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarCloudDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SonarCloudDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zbm/demo/sonarcloud/SonarCloudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zbm.demo.sonarcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarCloudDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
